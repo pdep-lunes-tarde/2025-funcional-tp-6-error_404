5ba580cd339e9bec62202aee1e1a86863fd6500a
urigg01 <ugraneroganem@frba.utn.edu.ar>
2025-05-11 17:01:31 -0300

    Inicio

--- src/Library.hs
+++ src/Library.hs
@@ -2,7 +2,7 @@ module Library where
 import PdePreludat
 
 data Ingrediente =
-    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
+    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras | BaconDeTofu
     deriving (Eq, Show)
 
 precioIngrediente Carne = 20
@@ -12,9 +12,30 @@ precioIngrediente Cheddar = 10
 precioIngrediente Pollo =  10
 precioIngrediente Curry = 5
 precioIngrediente QuesoDeAlmendras = 15
+precioIngrediente BaconDeTofu = 12
 
 data Hamburguesa = Hamburguesa {
     precioBase :: Number,
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
 
+-- Hamburguesa de ejemplo
+cuartoDeLibra :: Hamburguesa
+cuartoDeLibra = Hamburguesa 20 ["Pan", "Carne", "Cheddar", "Pan"]
+
+--Precio Final
+precioFinal :: Hamburguesa -> Number
+precioFinal (Hamburguesa precioBase ingredientes) = precioBase + sum . map precioIngrediente . ingredientes
+
+--Agrandar
+
+agrandar :: Ingrediente -> Hamburguesa -> Hamburguesa
+agrandar ingrediente (Hamburguesa unPrecio ingredientes) = Hamburguesa (unPrecio + precioIngrediente ingrediente) ()
+     
+
+agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
+agregarIngrediente ingr burga = 
+
+descuento = implementame
+
+pdepBurger = implementame
